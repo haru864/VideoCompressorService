155dca1324461eb9247a3bbbf2389ca365aba3e7
haru <kawachigenji95@gmail.com>
2023-10-27 01:00:53 +0900

    Implement exchange of compression methods before sending video data.

--- src/client.py
+++ src/client.py
@@ -1,8 +1,22 @@
+import struct
+from typing import Any
 import socket
-import os
-
+import json
 
 BUFFER_SIZE: int = 1024
+PREFIX_LENGTH: int = 4
+operation_list: list[str] = [
+    "compress",
+    "change_resolution",
+    "change_aspect_ratio",
+    "convert_to_audio",
+    "trim_by_time_range",
+]
+compress_level_to_bitrate_multiplier: dict[str, int] = {
+    "low": 0.9,
+    "medium": 0.7,
+    "high": 0.5,
+}
 
 
 def main():
@@ -14,20 +28,67 @@ def main():
             break
         except Exception:
             print("数値を入力してください")
-
     print(f"Connecting to ({server_address}, {server_port})")
     tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp_client.connect((server_address, server_port))
 
-    video_file_path: str = (
-        "/home/haru/project/Recursion/VideoCompressorService/test/input/sample.mp4"
-    )
-    with open(video_file_path, 'rb') as video_file:
+    request_data: dict[str, Any] = {}
+    print("Choose the operation number.")
+    for index, operation in enumerate(operation_list):
+        print(f" {index+1}.{operation}")
+    while True:
+        try:
+            user_input: str = int(input("operation number: "))
+            if user_input < 1 or user_input > len(operation_list):
+                print(f"1~{len(operation_list)} is available")
+                continue
+            request_data["operation"] = operation_list[user_input - 1]
+            break
+        except Exception:
+            print("Please enter operation number")
+
+    if request_data["operation"] == "compress":
+        print("Choose compress level.")
+        for index, compress_level in enumerate(
+            compress_level_to_bitrate_multiplier.keys()
+        ):
+            print(f" {index+1}.{compress_level}")
         while True:
-            bytes_read = video_file.read(BUFFER_SIZE)
-            if not bytes_read:
+            try:
+                user_input: str = int(input("compress level: "))
+                if user_input < 1 or user_input > len(
+                    compress_level_to_bitrate_multiplier
+                ):
+                    print(f"1~{len(compress_level_to_bitrate_multiplier)} is available")
+                    continue
+                request_data["compress_level"] = list(
+                    compress_level_to_bitrate_multiplier.values()
+                )[user_input - 1]
                 break
-            tcp_client.sendall(bytes_read)
+            except Exception:
+                print("Please enter compress level")
+
+    json_str: str = json.dumps(request_data)
+    data: bytes = json_str.encode()
+    data_length_prefix: bytes = struct.pack("!I", len(data))
+    tcp_client.sendall(data_length_prefix + data)
+
+    length_data: bytes = tcp_client.recv(PREFIX_LENGTH)
+    (response_data_length,) = struct.unpack("!I", length_data)
+    response_data: bytes = tcp_client.recv(response_data_length)
+    response_data_json: dict[str, int] = json.loads(response_data.decode())
+    print(response_data_json)
+
+    # video_file_path: str = (
+    #     "/home/haru/project/Recursion/VideoCompressorService/test/input/sample.mp4"
+    # )
+    # with open(video_file_path, "rb") as video_file:
+    #     while True:
+    #         bytes_read = video_file.read(BUFFER_SIZE)
+    #         if not bytes_read:
+    #             break
+    #         tcp_client.sendall(bytes_read)
+
     tcp_client.close()
 
 
--- src/server.py
+++ src/server.py
@@ -1,24 +1,55 @@
+import struct
 from typing import Any
 import asyncio
 import socket
 import json
 import os
-
+import ffmpeg
 
 BUFFER_SIZE: int = 1024
+PREFIX_LENGTH: int = 4
 
 
 async def handle_client(client, loop) -> None:
-    video_path: str = os.path.join(
-        os.path.dirname(os.path.dirname(__file__)),
-        "tmp/before_process/output_video.mp4",
-    )
-    with open(video_path, "wb") as video_file:
-        while data := await loop.sock_recv(client, BUFFER_SIZE):
-            if not data:
-                break
-            video_file.write(data)
+    length_data: bytes = await loop.sock_recv(client, PREFIX_LENGTH)
+    (length,) = struct.unpack("!I", length_data)
+    data: bytes = await loop.sock_recv(client, length)
+    json_data = json.loads(data.decode())
+    print(json_data)
+
+    try:
+        requested_operation: str = json_data["operation"]
+        if requested_operation == "compress":
+            ompress_level: float = json_data["compress_level"]
+        response_str: str = json.dumps({"status": 0})
+    except Exception as e:
+        print(e)
+        response_str: str = json.dumps({"status": 1})
+    data: bytes = response_str.encode()
+    data_length_prefix: bytes = struct.pack("!I", len(data))
+    await loop.sock_sendall(client, data_length_prefix + data)
+
+    # video_path: str = os.path.join(
+    #     os.path.dirname(os.path.dirname(__file__)),
+    #     "tmp/before_process/output_video.mp4",
+    # )
+    # with open(video_path, "wb") as video_file:
+    #     while data := await loop.sock_recv(client, BUFFER_SIZE):
+    #         if not data:
+    #             break
+    #         video_file.write(data)
+    # try:
+    #     probe: dict[Any, Any] = ffmpeg.probe(video_path)
+    #     print(probe)
+    #     video_info = next(s for s in probe["streams"] if s["codec_type"] == "video")
+    #     default_bitrate = video_info["bit_rate"]
+    #     print(f"default_bitrate -> {default_bitrate}")
+    # except Exception as e:
+    #     print(e)
+    #     await loop.sock_sendall(client, b"Content must be video")
+
     client.close()
+    return None
 
 
 async def run_tcp_server() -> None:
